7b1ae1e61dddfa0243d6025da4f51eddf74c52f3
Jack Nehme <jack.nehme@gmail.com>
2018-06-27 22:28:33 -0700

    added state to user maskUserName

--- APM/src/app/user/user.module.ts
+++ APM/src/app/user/user.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
 import { LoginComponent } from './login.component';
+import { reducer } from './state/user.reducer';
+import { StoreModule } from '@ngrx/store';
 
 const userRoutes: Routes = [
   { path: 'login', component: LoginComponent }
@@ -12,7 +14,8 @@ const userRoutes: Routes = [
 @NgModule({
   imports: [
     SharedModule,
-    RouterModule.forChild(userRoutes)
+    RouterModule.forChild(userRoutes),
+    StoreModule.forFeature('users', reducer)
   ],
   declarations: [
     LoginComponent
